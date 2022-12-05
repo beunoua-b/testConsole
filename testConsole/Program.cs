67ea466463cc9d70cf17b84e0b065f37094a682b
benbo <benbo@PC-BEUNOUA>
2022-12-05 10:56:26 +0100

    Ajoutez des fichiers projet.

---
+++ testConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace testConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Premier test");
+            Console.ReadLine();
+        }
+    }
+}
